2c89cbf9845af59bcb82d04d7af56a768c6297f5
JennyDelaney <>
2023-05-24 17:00:10 +0000

    updated allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -28,6 +28,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     if created:
-        UserProfile.objects.create(user=insteance)
+        UserProfile.objects.create(user=instance)
     # Existing users:just save the Profile
     instance.userprofile.save()
